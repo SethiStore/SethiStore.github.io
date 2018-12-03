f8160f5c4019486ee5ec8364d8f049ff0cbfa146
TechnoBeat99 <mohitsethi.32@gmail.com>
2018-12-03 23:08:44 +0530

    Added A template

---
+++ admin_area/includes/db.php
@@ -0,0 +1,6 @@
+<?php
+$con = mysqli_connect("localhost","contactw_ecommer","061695","contactw_ecommerce");
+if(mysqli_connect_errno()){
+    echo "The Connection was not established: " . mysqli_connect_error();
+}
+?>
